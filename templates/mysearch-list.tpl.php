400b7329ddf055de962954fa9251828279c409e3
Dmitriy Buzinov <dbuzinov@geeksforless.net>
2016-10-11 20:37:42 +0300

    add pagination and clean code style based on https://www.drupal.org/docs/develop/standards/coding-standards

--- templates/mysearch-list.tpl.php
+++ templates/mysearch-list.tpl.php
@@ -1,10 +1,13 @@
-<h2>Search for: <?php print ($search); ?></h2>
-<ul>
-<?php
-foreach($nodes as $node) {
-?>
-<li><?php print l($node['title'], "node/" . $node['nid']); ?></li>
-<?php
-}
-?>
-</ul>
+<h2><?php print $lable?>: <?php print ($search); ?></h2>
+<?php 
+if(sizeof($nodes) > 0) { ?>
+  <ul>
+    <?php foreach($nodes as $node) { ?>
+    <li><?php print l($node['title'], "node/" . $node['nid']); ?></li>
+    <?php } ?>
+  </ul>
+<?php 
+} 
+else { 
+  print $empty;
+} 
